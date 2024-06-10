89f3038089d2d5ab5f6bda1e9c5e87b998060b49
rayssa-nn <lamasrayssa@gmail.com>
2024-06-10 14:04:52 -0300

    estoy aqui

---
+++ script.js
@@ -0,0 +1,9 @@
+console.log("aula de git");
+class carro{
+    constructor(tipo, capacidade){
+        this.tipo = capacidade;
+        this.capacidade = capacidade;
+    }
+}
+
+const gol = new carro("carro",5)
